a02818e8ceee37d6964d358762df69cc21496738
MitinNV <tyuiu.mitinnv@outlook.com>
2024-11-11 17:10:28 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint5.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MitinNV.Sprint5.Task3.V12.Lib
+{
+    public class DataService : ISprint5Task3V12
+    {
+        public string SaveToFileTextData(int x)
+        {
+            return Convert.ToString(Math.Round(Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2)), 3));
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint5.Task3.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MitinNV.Sprint5.Task3.V12.Lib;
+namespace Tyuiu.MitinNV.Sprint5.Task3.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Потоковый метод записи данных в бинарный файл                           *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дано выражение вычислить его значение при x = 3, результат сохранить в би*");
+            Console.WriteLine("*инарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            var result = ds.YOURFUNCTION(a, b);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
