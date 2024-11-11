54f5463db4b6b51e3d4b4511e0e8689d4db57782
MitinNV <tyuiu.mitinnv@outlook.com>
2024-11-11 17:14:09 +0500

    .

--- Tyuiu.MitinNV.Sprint5.Task3.V12.Lib/DataService.cs
+++ Tyuiu.MitinNV.Sprint5.Task3.V12.Lib/DataService.cs
@@ -5,8 +5,11 @@ namespace Tyuiu.MitinNV.Sprint5.Task3.V12.Lib
     public class DataService : ISprint5Task3V12
     {
         public string SaveToFileTextData(int x)
-        {
-            return Convert.ToString(Math.Round(Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2)), 3));
+        { 
+            double calculation = Math.Round(Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2)), 3);
+            byte[] byteArray = BitConverter.GetBytes(calculation);
+
+            return Convert.ToBase64String(byteArray);
         }
     }
 }
