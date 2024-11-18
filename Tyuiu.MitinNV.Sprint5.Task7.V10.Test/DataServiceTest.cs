25fedacf1eed61ca57e8bc6d7abde1dcd0567a53
MitinNV <tyuiu.mitinnv@outlook.com>
2024-11-18 18:29:23 +0500

    .

--- Tyuiu.MitinNV.Sprint5.Task7.V10.Test/DataServiceTest.cs
+++ Tyuiu.MitinNV.Sprint5.Task7.V10.Test/DataServiceTest.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.MitinNV.Sprint5.Task7.V10.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            var varForTest = 0;
-            Assert.AreEqual(varForTest, ds.YOURFUNCTION());
+            var varForTest = "PATH";
+            Assert.AreEqual(varForTest, ds.LoadDataAndSave("C:\\DataSprint5\\InPutDataFileTask7V10.txt"));
         }
     }
 }
